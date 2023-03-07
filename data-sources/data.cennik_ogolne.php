791483f1c10ddd239f5e775520e89e9519a1fcdd
Olaf Schindler <studio@orkana39.pl>
2023-03-07 15:12:56 +0100

    Poprawka sortowania w cenniku

--- data-sources/data.cennik_ogolne.php
+++ data-sources/data.cennik_ogolne.php
@@ -10,7 +10,7 @@ class datasourcecennik_ogolne extends SectionDatasource
     public $dsParamREDIRECTONEMPTY = 'no';
     public $dsParamREDIRECTONFORBIDDEN = 'no';
     public $dsParamREDIRECTONREQUIRED = 'no';
-    public $dsParamSORT = 'system:id';
+    public $dsParamSORT = 'order';
     public $dsParamHTMLENCODE = 'no';
     public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
     
@@ -35,10 +35,10 @@ class datasourcecennik_ogolne extends SectionDatasource
             'name' => 'Cennik. Ogólne',
             'author' => array(
                 'name' => 'Olaf Schindler',
-                'website' => 'http://localhost/city-dent',
+                'website' => 'http://localhost/city-dent.pl',
                 'email' => 'studio@orkana39.pl'),
             'version' => 'Symphony 2.7.10',
-            'release-date' => '2021-11-29T14:48:35+00:00'
+            'release-date' => '2023-03-06T12:59:35+00:00'
         );
     }
 
--- data-sources/data.cennik_stomatologia_dziecieca.php
+++ data-sources/data.cennik_stomatologia_dziecieca.php
@@ -10,7 +10,7 @@ class datasourcecennik_stomatologia_dziecieca extends SectionDatasource
     public $dsParamREDIRECTONEMPTY = 'no';
     public $dsParamREDIRECTONFORBIDDEN = 'no';
     public $dsParamREDIRECTONREQUIRED = 'no';
-    public $dsParamSORT = 'system:id';
+    public $dsParamSORT = 'order';
     public $dsParamHTMLENCODE = 'no';
     public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
     
@@ -35,10 +35,10 @@ class datasourcecennik_stomatologia_dziecieca extends SectionDatasource
             'name' => 'Cennik. Stomatologia dziecięca',
             'author' => array(
                 'name' => 'Olaf Schindler',
-                'website' => 'http://localhost/city-dent',
+                'website' => 'http://localhost/city-dent.pl',
                 'email' => 'studio@orkana39.pl'),
             'version' => 'Symphony 2.7.10',
-            'release-date' => '2021-11-29T14:47:45+00:00'
+            'release-date' => '2023-03-06T13:06:47+00:00'
         );
     }
 
